89049adafaa77462cc6901ac0165c2a4618e192e
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-03 12:42:47 +0700

    refactoring: move query, mutation, resolvers and schema into separate folder

--- src/index.js
+++ src/index.js
@@ -1,65 +1,39 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 require('dotenv').config();
 
 const db = require('./db');
 const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 //run the server on a port specified in our .env file or port 4000 
 const port = process.env.PORT || 4000;
 
 const DB_HOST = process.env.DB_HOST;
 
-let notes = [
-    { id: '1', content: 'This is a note', author: 'Adam Scott' },
-    { id: '2', content: 'This is another note', author: 'Harlow Everly' },
-    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
-]
+// let notes = [
+//     { id: '1', content: 'This is a note', author: 'Adam Scott' },
+//     { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+//     { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+// ]
 
 // construct a graphQL schema
-const typeDefs = gql `
-	type Note {
-		id: ID!
-		content: String!
-		author: String!
-	}
-
-	type Query {
-		notes: [Note!]!,
-		note(id: ID!): Note!
-	}
-
-	type Mutation {
-		newNote(content: String!, author: String!): Note!
-	}
-`;
 
 // provide resolver functions for our schema fields
-const resolvers = {
-    Query: {
-        notes: () => async () => {
-        	return await models.Note.find();
-        },
-        note: async (parent, args) => {
-        	return await models.Note.findById(args.id);
-        }
-    },
-    Mutation: {
-    	newNote: async (parent, args) => {
-    		return await models.Note.create({
-    			content: args.content,
-    			author: args.author
-    		})
-    	}
-    }
-};
 
 db.connect(DB_HOST);
 
 // apollo server setup
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+	typeDefs, 
+	resolvers,
+	context: () => {
+		return { models };
+	}
+});
 
 server.applyMiddleware({ app, path: '/api' });
 
