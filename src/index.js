c53da7b9b783805382d1506a0a65933f8fe47a72
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-03 10:29:45 +0700

    add : schema and db model to connect to mongodb database

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const app = express();
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
 
 //run the server on a port specified in our .env file or port 4000 
 const port = process.env.PORT || 4000;
+
+const DB_HOST = process.env.DB_HOST;
 
 let notes = [
     { id: '1', content: 'This is a note', author: 'Adam Scott' },
@@ -35,23 +41,24 @@ const typeDefs = gql `
 const resolvers = {
     Query: {
         hello: () => 'Hello World!',
-        notes: () => notes,
-        note: (parent, args) => {
-        	return notes.find(note => note.id === args.id)
+        notes: () => async () => {
+        	return await models.Note.find();
+        },
+        note: async (parent, args) => {
+        	return await models.Note.findById(args.id);
         }
     },
     Mutation: {
-    	newNote: (parent, args) => {
-    		let noteValue = {
-    			id: String(notes.length + 1),
+    	newNote: async (parent, args) => {
+    		return await models.Note.create({
     			content: args.content,
     			author: args.author
-    		};
-    		notes.push(noteValue);
-    		return noteValue;
+    		})
     	}
     }
 };
+
+db.connect(DB_HOST);
 
 // apollo server setup
 const server = new ApolloServer({ typeDefs, resolvers });
