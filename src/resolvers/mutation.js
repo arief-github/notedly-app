5923763f1dfa41cde14bb9d42810ece1182662fc
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-04 14:09:26 +0700

    add : User model, SignIn and SignUp mutations

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,13 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+    AuthenticationError,
+    ForbiddenError
+} = require('apollo-server-express');
+require('dotenv').config();
+
+const gravatar = require('../utils/gravatar');
+
 module.exports = {
     newNote: async (parent, args, { models }) => {
         return await models.Note.create({
@@ -8,25 +18,58 @@ module.exports = {
     deleteNote: async (parent, { id }, { models }) => {
         try {
             await models.Note.findOneAndRemove({ _id: id });
-        	return true;
+            return true;
         } catch (err) {
             return false;
         }
     },
-    updateNote: async(parent, { content, author, id }, { models }) => {
-    	return await models.Note.findOneAndUpdate(
-    		{
-    			_id: id
-    		},
-    		{
-    			$set: {
-    				content,
-    				author
-    			},
-    		},
-    		{
-    			new: true
-    		}
-    	)
+    updateNote: async (parent, { content, author, id }, { models }) => {
+        return await models.Note.findOneAndUpdate({
+            _id: id
+        }, {
+            $set: {
+                content,
+                author
+            },
+        }, {
+            new: true
+        })
+    },
+    signUp: async (parent, { username, email, password }, { models }) => {
+        email = email.trim().toLowerCase();
+        const hashed = await bcrypt.hash(password, 10);
+        const avatar = gravatar(email);
+        try {
+            const user = await models.User.create({
+                username,
+                email,
+                avatar,
+                password: hashed
+            });
+
+            return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        } catch (err) {
+            throw new Error('Error creating account');
+        }
+    },
+    signIn: async (parent, { username, email, password }, { models }) => {
+        if (email) {
+            email = email.trim().toLowerCase();
+        }
+
+        const user = await models.User.findOne({
+            $or: [{ email }, { username }]
+        })
+
+        if (!user) {
+            throw new AuthenticationError('Error signing in');
+        }
+
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new AuthenticationError('Error signing in');
+        }
+
+        return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     }
 }
