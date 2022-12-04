b2776bb6ace5cbd88329b811d61d54072f840425
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-04 11:10:00 +0700

    fix: indentation

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,8 +1,8 @@
 module.exports = {
-	notes: async (parent, args, { models }) => {
-		return await models.Note.find();
-	},
-	note: async (parent, args, { models }) => {
-		return await models.Note.findById(args.id)
-	}
+    notes: async (parent, args, { models }) => {
+        return await models.Note.find();
+    },
+    note: async (parent, args, { models }) => {
+        return await models.Note.findById(args.id);
+    }
 };
